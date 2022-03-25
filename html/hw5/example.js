9e2a3a101ffbeff3d132d14ebb99c3571fe55cc4
Michael A. Milazzo <1437369+milazzom@users.noreply.github.com>
2022-03-24 21:20:48 -0400

    Updated some changes to examples
    Added the latest edits to the hw5 example files.

--- html/hw5/example.js
+++ html/hw5/example.js
@@ -4,10 +4,30 @@ function runOnLoad() {
 }
 
 function getNames() {
-
+    var select = document.getElementById("exampleDropDown");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        
+        if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+            console.log("This request succeeded!", httpRequest.responseText);
+            var names = httpRequest.responseText.split("\n");
+            console.log("There are " + names.length + " names returned!");
+            
+            for(var i = 0; i < names.length; i++) {
+                var currentName = names[i];
+                var option = new Option(currentName, currentName);
+                select.add(option, false);
+                
+            }
+        }
+    };
+    httpRequest.open("GET", "http://u.arizona.edu/~milazzom/cscv337/sp19/hw5/babynames.php?type=list");
+    httpRequest.send();
 }
 
 function selectionMade() {
+    var selectedName = document.getElementById("exampleDropDown").value;
+    console.log("The selected name is: ", selectedName);
     getNameMeaning();
     getNameRankData();
 }
