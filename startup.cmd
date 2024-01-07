55d774d9ddfa8eba357180e0e67d2c7fdf2a7ea0
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2024-01-06 19:49:18 -0700

    Consolidates startup scripts
    This change adds the ability to download the sql init script in the same script as startup, so the database will always have it's dependencies.

--- startup.cmd
+++ startup.cmd
@@ -1,1 +1,7 @@
+if exist sql/init.sql (
+    rem SQL file already exists, not downloading.
+) else (
+    rem Downloading SQL file for CSCV337 database.s
+    curl -L http://u.arizona.edu/~milazzom/init.sql --output sql/init.sql
+)
 docker-compose up -d --build
--- startup.sh
+++ startup.sh
@@ -1,1 +1,6 @@
+if [[ -f "sql/init.sql" ]]; then
+    echo "SQL file already exists... skipping the download."
+else
+    curl -L http://u.arizona.edu/~milazzom/init.sql --output sql/init.sql
+fi
 docker-compose up -d --build
