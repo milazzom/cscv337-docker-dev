186f245bc34a0da72130351a91dcd8b694a9a6f9
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2024-02-14 19:11:36 -0700

    Adds Javascript + Fibonacci Example
    This commit adds the JavaScript example and the Fibonacci example we spoke about in class on 2/14/2024.

---
+++ examples/fibonacci.js
@@ -0,0 +1,15 @@
+function computeFibonacci() {
+    var nthNumber = document.getElementById("txtFib").value;
+    var fibonacciNumber = fibonacci(nthNumber);
+    document.getElementById("result").innerText = "F(" + nthNumber + ") = " + fibonacciNumber;
+}
+
+function fibonacci(number) {
+    if (number == 0) {
+        return 0;
+    }
+    if (number == 1) {
+        return 1;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
---
+++ examples/jsexample.js
@@ -0,0 +1,5 @@
+function clickMe() {
+    alert("The button was clicked.");
+    console.log("The button was clicked on the page.");
+    var myvariable = "this is a string";
+}
